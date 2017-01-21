10f3a266bf45be69c2a234892fa385291dbfa0a8
andela-osubair <oyindamola.subair@andela.com>
2017-01-21 13:56:52 +0100

    add upload functions

---
+++ src/app.js
@@ -0,0 +1,56 @@
+// (function () {
+
+// }());
+
+const app = angular.module('myApp', []);
+app.controller('IndexController', ($scope) => {
+  const invertedIndex = new InvertedIndex();
+  $scope.files = [];
+  $scope.documents = [];
+  let details = {};
+  $scope.fileNames = [];
+  $scope.indexShow = false;
+  $scope.showMessage = false;
+  $scope.uploadFile = () => {
+    const files = document.getElementById('files').files;
+    for (let il = 0; il < files.length; il += 1) {
+      const file = files[il];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const fileDetails = e.target.result;
+        const validate = invertedIndex.validateFile(fileDetails);
+        const firstValue = validate[0];
+        const secValue = validate[1];
+        if (!firstValue) {
+          $scope.$apply(() => {
+            $scope.message = secValue;
+            $scope.showMessage = true;
+          });
+        } else {
+          const jsonFile = JSON.parse(fileDetails);
+          details = {
+            name: file.name,
+            docs: jsonFile
+          };
+          $scope.$apply(() => {
+            if (!$scope.fileNames.includes(details.name)) {
+              $scope.documents.push(details);
+              $scope.indexShow = true;
+              $scope.message = secValue;
+            }
+          });
+        }
+      };
+      reader.readAsText(file);
+    }
+  };
+
+  $scope.indexFile = (file) => {
+    $scope.file = file.name;
+    $scope.docs = file.docs;
+    $scope.wordings = invertedIndex.createIndex(file.docs);
+    $scope.getty = invertedIndex.getIndex();
+    $scope.words = $scope.getty;
+    $scope.indexShow = false;
+  };
+});
